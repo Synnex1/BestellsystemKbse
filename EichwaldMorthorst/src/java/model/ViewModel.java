66411ffd352efd6afd2dda7eb03a7fa9802dbea7
Andi757 <andrejeichwald@gmx.de>
2017-02-08 15:51:04 +0100

    Tabelle

--- EichwaldMorthorst/src/java/model/ViewModel.java
+++ EichwaldMorthorst/src/java/model/ViewModel.java
@@ -3,15 +3,22 @@ package model;
 import java.io.Serializable;
 import javax.inject.Named;
 import controller.ProduktController;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 
 @Named(value = "vm")
 public class ViewModel implements Serializable {
+    @NotNull
     private String name;
+    @Min(value=1, message="Der Wert muss größer als 1 sein!")
+    @Max(value=10000, message="Es dürfen nicht mehr als 10000 Artikel auf einmal verkauft werden")
     private int anzahl;
-    
+    ProduktController pc;
     
     public ViewModel(){
+        this.pc = ProduktController.getInstance();
     }
 
     public String getName() {
@@ -31,6 +38,6 @@ public class ViewModel implements Serializable {
     }
     
     public void newProdukt(String name, int anzahl){
-        
+        pc.newProdukt(name, anzahl);
     }
 }
