51176d48da2a72c4f6d0e0754e63d807c2ce948c
Andi757 <andrejeichwald@gmx.de>
2017-02-09 23:13:50 +0100

    einlesen und in Tabelle anzeigen funktioniert jetzt

--- EichwaldMorthorst/src/java/model/ViewModel.java
+++ EichwaldMorthorst/src/java/model/ViewModel.java
@@ -5,12 +5,16 @@ import javax.inject.Named;
 import controller.ProduktController;
 import entity.Produkt;
 import java.util.List;
+import javax.enterprise.context.RequestScoped;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 
 @Named(value = "vm")
+@RequestScoped
 public class ViewModel implements Serializable {
+    @Size(min=2)
     private String name;
     @Min(value=1, message="Der Wert muss größer als 1 sein!")
     @Max(value=10000, message="Es dürfen nicht mehr als 10000 Artikel auf einmal verkauft werden")
