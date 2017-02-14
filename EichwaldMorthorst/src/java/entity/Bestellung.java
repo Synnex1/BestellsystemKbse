f73ef60b7acd9a9cf4f2300608b901a165c1f60a
Synnex1 <mike_morthorst@hotmail.de>
2017-02-14 12:17:51 +0100

    sfa
    sfa

--- EichwaldMorthorst/src/java/entity/Bestellung.java
+++ EichwaldMorthorst/src/java/entity/Bestellung.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,7 +27,7 @@ public class Bestellung implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String kunde;
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Bestellposten> bestellposten = new ArrayList<>();
 
     public Long getId() {
